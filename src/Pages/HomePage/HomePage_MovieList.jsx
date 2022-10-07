0683d8a0216795a1ad6e062eba3e68f28bc4731e
Nguyen Duong <xduong.plus84@gmail.com>
2022-10-08 02:18:48 +0700

    phan trang movie

--- src/Pages/HomePage/HomePage_MovieList.jsx
+++ src/Pages/HomePage/HomePage_MovieList.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Col, Divider, Pagination, Row } from "antd";
+import { Card, Col, Divider, Pagination, Row } from "antd";
 import ItemMovie from "../../Components/ItemMovie";
 import { quanLyPhimSer } from "../../Services/quanLyPhimSer";
 import { useSelector } from "react-redux";
@@ -9,17 +9,20 @@ import { useSelector } from "react-redux";
 // import "slick-carousel/slick/slick-theme.css";
 
 export default function HomePage_MovieList() {
-  const [currentPage, setCurrentPage] = useState(1);
-  // const { arrPhim } = useSelector((state) => state.quanLyPhimSer);
-  // console.log("arrPhim: ", arrPhim);
+  // // const { arrPhim } = useSelector((state) => state.quanLyPhimSer);
+  // // console.log("arrPhim: ", arrPhim);
   const [danhSachPhim, setDanhSachPhim] = useState([]);
+  let [dataRender, setDataRender] = useState([]);
 
   useEffect(() => {
     quanLyPhimSer
       .layDanhSachPhim()
       .then((res) => {
         // console.log("res: ", res);
-        setDanhSachPhim(res.data.content);
+        let content = res.data.content;
+        setDanhSachPhim(content);
+        setDataRender(content.slice(0, 4));
+        // console.log("res.data.content: ", res.data.content);
       })
       .catch((err) => {
         console.log(err);
@@ -27,7 +30,7 @@ export default function HomePage_MovieList() {
   }, []);
 
   const renderMovieItem = () => {
-    return danhSachPhim.map((item, index) => {
+    return dataRender.map((item, index) => {
       return (
         <Col key={index} className="gutter-row" span={6}>
           <div style={{ padding: "8px 0" }}>
@@ -37,22 +40,61 @@ export default function HomePage_MovieList() {
       );
     });
   };
+
+  // let data = [
+  //   { title: "Card title1", value: "Card content1" },
+  //   { title: "Card title2", value: "Card content2" },
+  //   { title: "Card title3", value: "Card content3" },
+  //   { title: "Card title4", value: "Card content4" },
+  //   { title: "Card title5", value: "Card content5" },
+  //   { title: "Card title6", value: "Card content6" },
+  //   { title: "Card title7", value: "Card content7" },
+  //   { title: "Card title8", value: "Card content8" },
+  //   { title: "Card title9", value: "Card content9" },
+  //   { title: "Card title10", value: "Card content10" },
+  //   { title: "Card title11", value: "Card content11" },
+  //   { title: "Card title12", value: "Card content12" },
+  //   { title: "Card title13", value: "Card content13" },
+  //   { title: "Card title14", value: "Card content14" },
+  //   { title: "Card title15", value: "Card content15" },
+  // ];
+
+  const handleChangePagination = (page, pageSize) => {
+    // setPageCurrent(page);
+    let dataSlice = danhSachPhim?.slice(
+      (page - 1) * pageSize,
+      (page - 1) * pageSize + pageSize
+    );
+    setDataRender(dataSlice);
+  };
+
+  // const renderItem = () => {
+  //   return dataRender.map((val) => (
+  //     <Card
+  //       title={val.title}
+  //       extra={<a href="#">More</a>}
+  //       style={{ width: 300 }}
+  //     >
+  //       <p>{val.value}</p>
+  //     </Card>
+  //   ));
+  // };
+
   return (
     <div className="container w-2/3 mx-auto py-5">
-      <Divider orientation="left">Danh sach phim</Divider>
-      <Row gutter={[16, 24]}>{renderMovieItem()}</Row>
-      {/* <Pagination
-        defaultCurrent={currentPage}
-        total={10}
-        pageSize={5}
+      {/* <Divider orientation="left">Danh sach phim</Divider> */}
+      <Row className="my-4" gutter={[16, 24]}>
+        {renderMovieItem()}
+      </Row>
+
+      <Pagination
+        defaultCurrent={1}
+        total={danhSachPhim.length}
+        pageSize={4}
         onChange={(page, pageSize) => {
-          console.log("pageSize: ", pageSize);
-          setCurrentPage(page);
-          setDanhSachPhim(danhSachPhim.splice(page * pageSize, pageSize));
-          console.log("danhSachPhim: ", danhSachPhim);
+          handleChangePagination(page, pageSize);
         }}
       />
-      <p>Đang ở trang {currentPage}</p> */}
     </div>
   );
 }
